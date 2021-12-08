47c646b5b854cb6290bd3473a8c8c0d521044c88
Richard Hanß <stu209194@mail.uni-kiel.de>
2021-12-08 12:50:37 +0100

    working furiganatool MVP!

--- visual.py
+++ visual.py
@@ -1,3 +1,45 @@
 from furigana.furigana import get_html
 
-print(get_html("私は猫ですよ"))
+import sys
+from PyQt5.QtWidgets import QWidget, QApplication, QVBoxLayout, QLineEdit
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+
+class Thing(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+
+        vbox = QVBoxLayout(self)
+
+        self.webEngineView = QWebEngineView()
+        self.loadPage()
+
+        self.textfield = QLineEdit()
+        self.textfield.textChanged.connect(lambda: self.webEngineView.setHtml(get_html(self.textfield.text())))
+        # self.textfield.textChanged.connect(lambda: print(self.textfield.text()))
+
+        vbox.addWidget(self.textfield)
+        vbox.addWidget(self.webEngineView)
+
+        self.setLayout(vbox)
+
+        self.setGeometry(300, 300, 350, 250)
+        self.setWindowTitle('furiganatool')
+        self.show()
+
+    def loadPage(self):
+            self.webEngineView.setHtml(get_html("ようこそ"))
+
+def main():
+
+    app = QApplication(sys.argv)
+    ex = Thing()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
